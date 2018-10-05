8a10415c1a27662c99b49278d0e79bad232aa872
zeljana <zeljana.bevanda7@gmail.com>
2018-10-05 22:14:17 +0200

    server started with 2 routes

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+app.get('/home', function (req, res) {
+    res.send("zeljanaaaa")
+})
+
+app.listen(3000, function() {
+    console.log("listennn")
+})
