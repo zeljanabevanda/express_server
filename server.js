95a3c9b2a2102129b27e8b3c95d8b29fab099849
zeljana <zeljana.bevanda7@gmail.com>
2018-10-13 18:20:11 +0200

    dodane rute za registraciju i korisnika

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const db = require('./db/index.js');
+const bodyParser = require('body-parser');
+const cors = require('cors')
 
 var express = require('express')
-var app = express()
+var app = express();
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+// app.use(cors);
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
@@ -12,13 +18,45 @@ app.get('/home', function (req, res) {
     res.send("zeljanaaaa")
 })
 
-db.none('INSERT INTO users(first_name) VALUES($1)', ['John'])
+db.none('INSERT INTO users(first_name) VALUES($1)', ['jure'])
     .then(() => {
-        // success;
+       console.log("gghgjgj") // success;
     })
     .catch(error => {
+        console.log('err')
         // error;
     });
+    
+    app.get('/user/:id', (req, res) => {
+        db.one({
+            text: 'SELECT * FROM users WHERE id = $1', // can also be a QueryFile object
+            values: [req.params.id]
+        })
+            .then(user => {
+                res.send(user)
+                console.log(user)
+            })
+            .catch(error => {
+                res.send('user not found')
+            });
+    
+    })
+
+    app.post('/register', (req, res) => {
+        console.log(req.body,'body')
+    }) 
+   
+
+
+
+
+
+
+
+
+
+
+
 app.listen(3000, function() {
     console.log("listennn")
 })
